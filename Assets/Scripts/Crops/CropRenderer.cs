c6053e4cccf576a7b1b48f498f022c5a306651e6
Sharundaar <rabiller.donatien@hotmail.fr>
2016-05-09 22:29:44 -0400

    Added crops and crops renderer. Imported the global clock.

---
+++ Assets/Scripts/Crops/CropRenderer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CropRenderer : MonoBehaviour {
+
+    private SpriteRenderer m_renderer;
+
+    [SerializeField]
+    private Sprite[] Stages;
+
+	// Use this for initialization
+	void Start () {
+        m_renderer = GetComponent<SpriteRenderer>();
+        if (Stages.Length == 0)
+        {
+            Debug.Log("<CropRenderer> You need to put at least one sprite in the renderer.");
+            enabled = false;
+            return;
+        }
+
+        m_renderer.sprite = Stages[0];
+
+        Crop crop = GetComponent<Crop>();
+        if(crop.GetStageCount() != Stages.Length)
+            Debug.Log("<CropRenderer> Not enough sprites to show all crops stages.");
+
+        crop.StageChanged += OnStageChanged;
+	}
+
+    private void OnStageChanged(object _sender, int _stage)
+    {
+        if(Stages.Length > 0)
+        {
+            m_renderer.sprite = Stages[Mathf.Min(_stage, Stages.Length - 1)];
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using DEngine.DayNightCycle;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager s_Instance = null;
+    public static GameManager Instance
+    {
+        get { return s_Instance; }
+        private set { s_Instance = value; }
+    }
+
+    private GlobalClock m_GlobalClock;
+
+    public GlobalClock GlobalClock
+    {
+        get { return m_GlobalClock; }
+    }
+
+	private void Awake()
+    {
+        m_GlobalClock = GetComponent<GlobalClock>();
+
+        s_Instance = this;
+    }
+}
